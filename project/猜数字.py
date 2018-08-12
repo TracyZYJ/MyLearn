6a2d7bde3b738ff94bc5b4e3e8517691563790d5
TracyZYJ <1779684237.qq.com>
2018-08-12 14:41:12 +0800

    project_1
    python猜数字游戏代码

---
+++ project/猜数字.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+print("---------test1--------")
+temp = input("猜猜我最喜欢的数字：")
+guess = int(temp)
+if guess == 6:
+     print("神算子啊你！")
+     print("哈哈，那也没有奖励")
+else:
+     print("不对不对，哎，友尽啦")
+print("溜了，不玩了")
